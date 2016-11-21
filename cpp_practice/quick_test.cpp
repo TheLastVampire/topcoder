147b44a17c4afa71f49dd55420427b721e7fdd2d
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-11-21 20:26:08 +0530

    virtual contest 376-2 on codeforces :) submitted A,B and F partially :):)

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -3,79 +3,54 @@
 #include <string>
 #include <map>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int temp_count = 1,temp_count_cross=1;
-
-vector<int> max_cross_sub_arr(vector<int>* a, int low, int mid, int high)
-{
-	cout << "counting cross: " << temp_count_cross++ << endl;
-	vector<int> result(3, -1);
-	int l_sum = -999, sum = 0, l_max, r_max;
-	for (int i = mid; i >= low; i--)
-	{
-		sum += (*a)[i];
-		if (sum > l_sum)
-		{
-			l_sum = sum;
-			l_max = i;
-		}
-	}
-
-	int r_sum = -999;
-	sum = 0;
-	for (int i = mid + 1; i <= high; i++)
-	{
-		sum += (*a)[i];
-		if (sum > r_sum)
-		{
-			r_sum = sum;
-			r_max = i;
-		}
-	}
-	result[0] = l_max;
-	result[1] = r_max;
-	result[2] = l_sum + r_sum;
-	return result;
-}
-
-vector<int> max_sub_arr(vector<int>* a, int low, int high)
-{
-	cout << "counting max: " << temp_count++ << endl;
-	vector<int>l, r, cross(3, -1);
-	if (high == low)
-	{
-		r[0] = low;
-		r[1] = high;
-		r[2] = (*a)[low];
-		return r;
-	}
-	else
-	{
-		int mid = (low + high) / 2;
-
-		l = max_sub_arr(a, low, mid + 1);
-		r = max_sub_arr(a, mid + 1, high);
-		cross = max_cross_sub_arr(a, low, mid, high);
-
-		if (l[2] >= r[2] && l[2] >= cross[2])
-			return l;
-		else
-			if (r[2] >= l[2] && r[2] >= cross[2])
-				return r;
-			else
-				return cross;
-
-	}
-}
 
 int main()
 {
-	vector<int> a = { 13,-3,-25,20, -3,-16,-23,18,20,-7,12,-5,-22,15,-4,7 };
-	int low = 0, mid = 7, high = 15;
-	vector<int> solution = max_sub_arr(&a, low, high);
-	cout << solution[0] << " " << solution[1] << " " << solution[2];
+	int n = 0, sum = 0, result = 0;
+	cin >> n;
+	vector<int>a;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
+	}
+	sort(a.begin(), a.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i != 0 && a[i - 1] == a[i])
+			continue;
+		sum += a[i];
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[j] % a[i] == 0)
+			{
+				sum += a[j];
+			}
+			else
+			{
+				if (a[j] > a[i])
+				{
+					sum += (a[i] * (a[j] / a[i]));
+				}
+			}
+		}
+		if (sum > result)
+		{
+			result = sum;
+			sum = 0;
+		}
+		else
+		{
+			sum = 0;
+		}
+	}
+	cout << result;
 
 	system("pause");
 	return 0;
