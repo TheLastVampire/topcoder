55b43e12bdfa669b9f7e59629ef22393a524dd92
AJAY KUMAR <AJAY KUMAR@CONTINUUM>
2016-11-21 10:38:09 +0530

    trying divide & conquer

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -6,46 +6,77 @@
 
 using namespace std;
 
-bool is_prime(int n)
+int temp_count = 1,temp_count_cross=1;
+
+vector<int> max_cross_sub_arr(vector<int>* a, int low, int mid, int high)
 {
-	int sqrt_n = ceil(sqrt(n));
-	if (n <= 1)
-		return false;
-	if (n == 2 || n == 3)
-		return true;
-	if (n % 2 == 0)
-		return false;
-	else
+	cout << "counting cross: " << temp_count_cross++ << endl;
+	vector<int> result(3, -1);
+	int l_sum = -999, sum = 0, l_max, r_max;
+	for (int i = mid; i >= low; i--)
 	{
-		for (int i = 3; i <= sqrt_n; i += 2)
+		sum += (*a)[i];
+		if (sum > l_sum)
 		{
-			if (n%i == 0)
-				return false;
+			l_sum = sum;
+			l_max = i;
 		}
 	}
-	return true;
+
+	int r_sum = -999;
+	sum = 0;
+	for (int i = mid + 1; i <= high; i++)
+	{
+		sum += (*a)[i];
+		if (sum > r_sum)
+		{
+			r_sum = sum;
+			r_max = i;
+		}
+	}
+	result[0] = l_max;
+	result[1] = r_max;
+	result[2] = l_sum + r_sum;
+	return result;
+}
+
+vector<int> max_sub_arr(vector<int>* a, int low, int high)
+{
+	cout << "counting max: " << temp_count++ << endl;
+	vector<int>l, r, cross(3, -1);
+	if (high == low)
+	{
+		r[0] = low;
+		r[1] = high;
+		r[2] = (*a)[low];
+		return r;
+	}
+	else
+	{
+		int mid = (low + high) / 2;
+
+		l = max_sub_arr(a, low, mid + 1);
+		r = max_sub_arr(a, mid + 1, high);
+		cross = max_cross_sub_arr(a, low, mid, high);
+
+		if (l[2] >= r[2] && l[2] >= cross[2])
+			return l;
+		else
+			if (r[2] >= l[2] && r[2] >= cross[2])
+				return r;
+			else
+				return cross;
+
+	}
 }
 
 int main()
 {
-	unsigned int n = 0;
-	cin >> n;
-	for (int i = 4; i <= n / 2 ; i++)
-	{
-		if (is_prime(i))
-			continue;
-		if (!is_prime(n - i))
-		{
-			cout << i << " " << n - i;
-			break;
-		}
-	}
+	vector<int> a = { 13,-3,-25,20, -3,-16,-23,18,20,-7,12,-5,-22,15,-4,7 };
+	int low = 0, mid = 7, high = 15;
+	vector<int> solution = max_sub_arr(&a, low, high);
+	cout << solution[0] << " " << solution[1] << " " << solution[2];
 
-	//for(int i=1;i<=n;i++)
-	//	if (is_prime(i))
-	//	{
-	//		cout << i << " ";
-	//	}
 	system("pause");
 	return 0;
 }
