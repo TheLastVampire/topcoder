0c7a8ff018af9baca56fa5efde612f38da8795e5
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-11-22 17:11:02 +0530

    hackerearth probem - "fredo is in a hurry"

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -7,50 +7,23 @@
 
 using namespace std;
 
+void solve()
+{
+	int n = 0;
+	cin >> n;
+	int x = floor((-3 + sqrt(9 + 8 * n)) / 2);
+	int u = 2 * n;
+	int w = ((x*(x + 1)) / 2) + 2 * (n - x);
+	int result = u < w ? u : w;
+	cout << result << endl;
+}
 
 int main()
 {
-	int n = 0, sum = 0, result = 0;
+	int n = 0;
 	cin >> n;
-	vector<int>a;
 	for (int i = 0; i < n; i++)
-	{
-		int temp;
-		cin >> temp;
-		a.push_back(temp);
-	}
-	sort(a.begin(), a.end());
-
-	for (int i = 0; i < n; i++)
-	{
-		if (i != 0 && a[i - 1] == a[i])
-			continue;
-		sum += a[i];
-		for (int j = i + 1; j < n; j++)
-		{
-			if (a[j] % a[i] == 0)
-			{
-				sum += a[j];
-			}
-			else
-			{
-				if (a[j] > a[i])
-				{
-					sum += (a[i] * (a[j] / a[i]));
-				}
-			}
-		}
-		if (sum > result)
-		{
-			result = sum;
-			sum = 0;
-		}
-		else
-		{
-			sum = 0;
-		}
-	}
-	cout << result;
+		solve();
 
 	system("pause");
 	return 0;
