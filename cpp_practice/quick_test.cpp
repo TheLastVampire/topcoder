e004b3ca44af93bd61971d2cebcd7f153757a432
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-11-23 19:58:40 +0530

    brute force for a new problem

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -7,28 +7,95 @@
 
 using namespace std;
 
-void solve()
+int gcd(int a, int b)
 {
-	unsigned long long n = 0;
-	cin >> n;
-	if (n == 1)
+	if (a == 0 || b == 0 || a < 0 || b < 0)
 	{
-		cout << 1 << endl;
-		return;
+		cout << "Both numbers should belong to +Z";
+		return -1;
 	}
-	unsigned long long x = floor((-3 + sqrt(9 + 8 * n)) / 2);
-	unsigned long long by_walk = (n*(n + 1)) / 2;
-	unsigned long long by_elev = 2 * (n - x);
-	unsigned long long result = by_walk<by_elev ? by_walk : by_elev;
-	cout << result << endl;
+	if (b%a == 0)
+		return a;
+	return gcd(b%a, a);
+}
+
+int look_left(vector<int>* a, int i)
+{
+	if (i == 1)
+		return -1;
+	for (int j = i - 1; j >= 1; j--)
+	{
+		if (gcd((*a)[i], (*a)[j]) > 1)
+		{
+			return j;
+		}
+	}
+	return -1;
+}
+
+int look_right(vector<int>* a, int i)
+{
+	int n = (*a).size() - 1;
+	if (i == n)    //if i = n, there is nothing on the right!
+		return -1;
+	for (int j = i + 1; j <= n; j++)
+	{
+		if (gcd((*a)[i], (*a)[j]) > 1)
+		{
+			return j;
+		}
+	}
+	return -1;
+}
+
+int closest_or_min(vector<int>* a, int i, int j_left, int j_right)
+{
+	int d_left = abs(i - j_left);
+	int d_right = abs(i - j_right);
+	if (!(d_left == d_right))
+	{
+		return (d_right < d_left ? j_right : j_left);
+	}
+	else
+	{
+		return ((*a)[j_right] < (*a)[j_left] ? j_right : j_left);
+	}
 }
 
 int main()
 {
-	/*int n = 0;
+	int n = 0;
 	cin >> n;
-	for (int i = 0; i < n; i++)*/
-		solve();
+	vector<int>a;
+	a.push_back(-999);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		a.push_back(temp);
+	}
+	//ignore a[0] !!!!
+	for (int i = 1; i <= n; i++)
+	{
+		int j_left = look_left(&a, i);
+		int j_right = look_right(&a, i);
+		if (j_left != -1 && j_right != -1)
+		{
+			cout << closest_or_min(&a, i, j_left, j_right) << " ";
+		}
+		else
+		{
+			if (j_left != -1)
+				cout << j_left << " ";
+			else
+			{
+				if (j_right != -1)
+					cout << j_right << " ";
+				else
+					cout << -1 << " ";
+			}
+		}
+	}
 
 	system("pause");
 	return 0;
