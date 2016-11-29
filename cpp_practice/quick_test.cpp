c9ed43285a7231c9a0df658a3c13bfc7bd88f71b
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-11-29 18:32:27 +0530

    "best divisor" hackerrank contest

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -4,125 +4,68 @@
 #include <map>
 #include <cmath>
 #include <algorithm>
+#include <set>
+#include <fstream>
 
 using namespace std;
 
-bool are_coprimes(unsigned a, unsigned b)
+unsigned digit_sum(unsigned n)
 {
-	if (((a | b) & 1) == 0)
-		return false;
-	while ((a & 1) == 0)
-		a >>= 1;
-	if (a == 1)
-		return true;
-	do
+	unsigned sum = 0;
+	while (n > 0)
 	{
-		while ((b & 1) == 0)
-			b >>= 1;
-		if (b == 1)
-			return true;
-		if (a > b)
-		{
-			unsigned t = a;
-			a = b;
-			b = t;
-		}
-		b -= a;
-	} while (b != 0);
-	return false;
-}
-
-int gcd(int a, int b)
-{
-	if (a == 0 || b == 0 || a < 0 || b < 0)
-	{
-		cout << "Both numbers should belong to +Z";
-		return -1;
+		sum += (n % 10);
+		n /= 10;
 	}
-	if (b%a == 0)
-		return a;
-	return gcd(b%a, a);
+	return sum;
 }
 
-int look_left(vector<int>* a, int i)
+void check(unsigned a, unsigned b, pair<unsigned, unsigned>* p)
 {
-	if (i == 1)
-		return -1;
-	for (int j = i - 1; j >= 1; j--)
+	pair<unsigned, unsigned>& result = *p;
+	unsigned sum_a = digit_sum(a);
+	unsigned sum_b = digit_sum(b);
+	unsigned greater = 0;
+	unsigned sum_greater = 0;
+	if (sum_a == sum_b)
 	{
-		if (!are_coprimes((*a)[i], (*a)[j]))
-		{
-			return j;
-		}
-	}
-	return -1;
-}
-
-int look_right(vector<int>* a, int i)
-{
-	int n = (*a).size() - 1;
-	if (i == n)    //if i = n, there is nothing on the right!
-		return -1;
-	for (int j = i + 1; j <= n; j++)
-	{
-		if (!are_coprimes((*a)[i], (*a)[j]) )
-		{
-			return j;
-		}
-	}
-	return -1;
-}
-
-int closest_or_min(vector<int>* a, int i, int j_left, int j_right)
-{
-	int d_left = abs(i - j_left);
-	int d_right = abs(i - j_right);
-	if (!(d_left == d_right))
-	{
-		return (d_right < d_left ? j_right : j_left);
+		greater = a < b ? a : b;
+		sum_greater = greater == a ? sum_a : sum_b;
 	}
 	else
 	{
-		return ((*a)[j_right] < (*a)[j_left] ? j_right : j_left);
+		greater = sum_a > sum_b ? a : b;
+		sum_greater = greater == a ? sum_a : sum_b;
+	}
+	if (sum_greater == result.second)
+	{
+		result.first == result.first < greater ? result.first : greater;
+	}
+	else
+	{
+		result.first = result.second > sum_greater ? result.first : greater;
+		result.second = result.first == a ? sum_a : sum_b;
 	}
 }
 
 int main()
 {
-	int n = 0;
+	unsigned n = 0;
 	cin >> n;
-	vector<int>a;
-	a.push_back(-999);
-	for (int i = 0; i < n; i++)
-	{		
-		int temp;
-		cin >> temp;
-		a.push_back(temp);
-	}
-	//ignore a[0] !!!!
-	for (int i = 1; i <= n; i++)
+	pair<unsigned, unsigned>result;
+	result.first = 1;
+	result.second = 1;
+	pair<unsigned, unsigned>* p = &result;
+	unsigned sq_root = floor(sqrt(n));
+	for (unsigned i = 1; i <= sq_root; i++)
 	{
-		int j_left = look_left(&a, i);
-		int j_right = look_right(&a, i);
-		if (j_left != -1 && j_right != -1)
+		if (n%i == 0)
 		{
-			cout << closest_or_min(&a, i, j_left, j_right) << " ";
-		}
-		else
-		{
-			if (j_left != -1)
-				cout << j_left << " ";
-			else
-			{
-				if (j_right != -1)
-					cout << j_right << " ";
-				else
-					cout << -1 << " ";
-			}
+			check(i, n / i, p);
 		}
 	}
 
-
+	cout << result.first;
 
 	system("pause");
 	return 0;
