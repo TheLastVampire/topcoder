049dcfa5a71f58c9482010a130dc9f00037fdf56
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-12-13 20:25:13 +0530

    solving "edit distance" under dp, gfg

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -7,77 +7,102 @@
 #include <set>
 #include <fstream>
 #include <time.h>
+#include <stack>
 
 using namespace std;
 
 ifstream read;
 ofstream write;
 
-int get_max(int a, int b)
+int max(int a, int b)
 {
 	return a > b ? a : b;
 }
 
-void solve()
+string lcs(string* str1, string* str2)
 {
-	int len_x = 0, len_y = 0, max = 0;
-	cin >> len_x >> len_y;
-	vector<char>x,y;
-	x.push_back(' ');
-	y.push_back(' ');
-	for (int i = 0; i < len_x; i++)
-	{
-		char temp;
-		cin >> temp;
-		x.push_back(temp);
-	}
-	for (int i = 0; i < len_y; i++)
-	{
-		char temp;
-		cin >> temp;
-		y.push_back(temp);
-	}
-	//done reading x and y
+	string& s1 = *str1;
+	string& s2 = *str2;
+
+	string x = "?" + s1;
+	string y = "?" + s2;
+
 	vector<vector<int>>c;
-	for (int i = 0; i <= len_x; i++)
+	vector<vector<char>>m;
+	for (int i = 0; i < x.length(); i++)
 	{
-		vector<int>temp_vec(len_y+1, 0);
-		c.push_back(temp_vec);
+		vector<int> temp(y.length(), 0);
+		c.push_back(temp);
 	}
-	//done building c[][]
-	for(int i=1;i<=len_x;i++)
-		for (int j = 1; j <= len_y; j++)
+	for (int i = 0; i < x.length(); i++)
+	{
+		vector<char> temp(y.length(), ' ');
+		m.push_back(temp);
+	}
+	for (int i = 1; i < x.length(); i++)
+	{
+		for (int j = 1; j < y.length(); j++)
 		{
 			if (x[i] == y[j])
 			{
-				c[i][j] = c[i - 1][j - 1] + 1;
-				max = get_max(max, c[i][j]);
+				c[i][j] = max(c[i - 1][j], c[i][j - 1]) + 1;
+				m[i][j] = '\\';
 			}
 			else
 			{
-				if (c[i - 1][j] >= c[i][j - 1])
+				if (c[i - 1][j] > c[i][j - 1])
 				{
 					c[i][j] = c[i - 1][j];
-					max = get_max(max, c[i][j]);
+					m[i][j] = '^';
 				}
 				else
 				{
-					c[i][j] = c[i][j - 1];
-					max = get_max(max, c[i][j]);
+					c[i][j] = c[i][j-1];
+					m[i][j] = '<';
 				}
 			}
 		}
-	cout << max << endl;
+	}
+
+	//done building c,m
+	string z="";
+	stack<char> st;
+	for (int i = x.length() - 1, j = y.length() - 1; m[i][j] != ' ';)
+	{
+		if (m[i][j] == '\\')
+		{
+			st.push(x[i]);
+			i--;
+			j--;
+		}
+		else
+		{
+			if (m[i][j] == '^')
+			{
+				i--;
+			}
+			else
+			{
+				j--;
+			}
+		}
+	}
+	//done filling stack
+	while (!st.empty())
+	{
+		z.push_back(st.top());
+		st.pop();
+	}
+	cout << s1 << endl << s2 << endl << z;
+	return z;
 }
 
 int main()
 {
-	int n = 0;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		solve();
-	}
+	string str1 = "klsrmnp", str2 = "serimno";
+	string z = lcs(&str1, &str2);
+
+	//cout << z;
 
 	system("pause");
 	return 0;
