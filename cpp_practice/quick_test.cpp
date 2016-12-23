751eb3a4365d4e01c41ab7b614cbd6abfc34d0c6
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2016-12-23 17:37:43 +0530

    solved pageno. problem week of code 27 hr

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -14,81 +14,30 @@ using namespace std;
 ifstream read;
 ofstream write;
 
-bool process_guid(string* str, int pos, vector<string>* v, set<string>* s)
+int min(int a, int b)
 {
-	string& line = *str;
-	vector<string>& guid = *v;
-	set<string>& guid_set = *s;
-
-	string gu = "";
-	for (int i = pos + 1; i < line.length() && line[i] != '}'; i++)
-	{
-		gu.append(1, line[i]);
-	}
-
-	if (guid_set.find(gu) != guid_set.end())
-	{
-		cout << "DUPLICATE " << gu << endl;
-		return true;
-	}
-	else
-	{
-		guid.push_back(gu);
-		guid_set.insert(gu);
-		return false;
-	}
-
+	return a < b ? a : b;
 }
 
-bool process_line(string* str, vector<string>* v, set<string>* s)
+int dis(int a, int b)
 {
-	string& line = *str;
-	vector<string>& guid = *v;
-	set<string>& guid_set = *s;
-
-	if (line.find('{') != line.npos)
-	{
-		for (int ind = 0; ind < line.length(); ind++)
-		{
-			if (line[ind] == '{')
-				if (process_guid(&line, ind, &guid, &guid_set))
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-		}
-	}
+	int k1 = 0, k2 = 0;
+	if (a % 2 == 0)
+		k1 = (a + 2) / 2;
 	else
-	{
-		return false;
-	}
+		k1 = (a + 1) / 2;
+	if (b % 2 == 0)
+		k2 = (b + 2) / 2;
+	else
+		k2 = (b + 1) / 2;
+	return abs(k1 - k2);
 }
 
 int main()
 {
-	vector<string>guid;
-	set<string>guid_set;
-	read.open("test.txt");
-	int i = 1;
-	while (!read.eof())
-	{
-		string line = "";
-		getline(read, line);
-		cout << "line " << i << " ..." << endl;
-		if (process_line(&line, &guid, &guid_set))
-		{
-			char temp;
-			cin >> temp;
-			break;
-
-		}
-		i++;
-	}
-
-	read.close();
+	int n = 0, p = 0, a = 1;
+	cin >> n >> p;
+	cout << min(dis(a, p), dis(p, n));
 
 
 	system("pause");
