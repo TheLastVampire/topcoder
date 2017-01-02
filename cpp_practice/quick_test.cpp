ac087a13de77ea25bcd87037f726521a72c974e2
TheLastVampire <ajaykumarsunki.poker@gmail.com>
2017-01-02 20:17:22 +0530

    putting proclevel3 items in quotations to make them strings for relevant images url scraping

--- cpp_practice/quick_test.cpp
+++ cpp_practice/quick_test.cpp
@@ -14,32 +14,39 @@ using namespace std;
 ifstream read;
 ofstream write;
 
-int min(int a, int b)
-{
-	return a < b ? a : b;
-}
-
-int dis(int a, int b)
-{
-	int k1 = 0, k2 = 0;
-	if (a % 2 == 0)
-		k1 = (a + 2) / 2;
-	else
-		k1 = (a + 1) / 2;
-	if (b % 2 == 0)
-		k2 = (b + 2) / 2;
-	else
-		k2 = (b + 1) / 2;
-	return abs(k1 - k2);
-}
-
 int main()
 {
-	int n = 0, p = 0, a = 1;
-	cin >> n >> p;
-	cout << min(dis(a, p), dis(p, n));
+	read.open("test.txt");
+	vector<string>lines;
+	int i = 1;
+	while (!read.eof())
+	{
+		cout << "reading line " << i << endl;
+		string line = "";
+		getline(read, line);
+		lines.push_back(line);
+		i++;
+	}
+	read.close();
+	i = 1;
+	for (vector<string>::iterator it = lines.begin(); it != lines.end(); it++,i++)
+	{
+		cout << "changing line " << i << endl;
+		(*it).insert(0, "'");
+		(*it) += "'";
+		
+	}
+	i = 1;
+	write.open("result.txt");
+	write.clear();
+	for (vector<string>::iterator it = lines.begin(); it != lines.end(); it++,i++)
+	{
+		cout << "writimg line " << i << endl;
+		write << *it << endl;
 
-
+	}
+	write.close();
+	cout << "done!" << endl;
 	system("pause");
 	return 0;
 }
